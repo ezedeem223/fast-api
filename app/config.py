43b167ca19836be19d02a04e04c0d9d6e879bf6d
ezedeem223 <za9699185@gmail.com>
2024-09-02 03:09:05 +0300

    Frالر

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     database_name: str
     database_username: str
     secret_key: str
-    algorithm: str = "RS256"  # استخدم RS256 كخوارزمية التشفير
+    algorithm: str = "RS256"
     access_token_expire_minutes: int
     google_client_id: str = "default_google_client_id"
     google_client_secret: str = "default_google_client_secret"
@@ -20,16 +20,13 @@ class Settings(BaseSettings):
     mail_from: EmailStr
     mail_port: int
     mail_server: str
-
-    # إضافة الخاصية المفقودة
     vault_token: str = "your_vault_token"
 
-    rsa_private_key: str = "C:/Users/kglou/Desktop/fastapi/private_key.pem"
-    rsa_public_key: str = "C:/Users/kglou/Desktop/fastapi/public_key.pem"
+    # تغيير هذه السطور لقراءة المفاتيح من المتغيرات البيئية
+    rsa_private_key: str
+    rsa_public_key: str
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
 settings = Settings()
