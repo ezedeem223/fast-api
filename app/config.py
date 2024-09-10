1a35104521b0c8ccb67245bf1a2212f99436ee76
ezedeem223 <za9699185@gmail.com>
2024-09-10 11:23:48 +0300

    ezed

--- app/config.py
+++ app/config.py
@@ -21,21 +21,20 @@ class Settings(BaseSettings):
     mail_from: EmailStr
     mail_port: int
     mail_server: str
-    vault_token: str = "your_vault_token"
-    rsa_private_key: str
-    rsa_public_key: str
+    # vault_token: str = "your_vault_token"
+    rsa_private_key_path: str = "C:/Users/kglou/Desktop/fastapi/private_key.pem"
+    rsa_public_key_path: str = "C:/Users/kglou/Desktop/fastapi/public_key.pem"
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.rsa_private_key = self._read_key_file("private_key.pem")
-        self.rsa_public_key = self._read_key_file("public_key.pem")
+        self.rsa_private_key = self._read_key_file(self.rsa_private_key_path)
+        self.rsa_public_key = self._read_key_file(self.rsa_public_key_path)
 
     def _read_key_file(self, filename):
-        file_path = os.path.join(os.path.dirname(__file__), "..", filename)
         try:
-            with open(file_path, "r") as file:
+            with open(filename, "r") as file:
                 return file.read().strip()
         except FileNotFoundError:
             raise ValueError(f"Key file not found: {filename}")
