a5c3e9d70d4bdee0bbdefb69c047735ae28ee42c
ezedeem223 <za9699185@gmail.com>
2024-09-10 13:11:05 +0300

    ezج

--- app/config.py
+++ app/config.py
@@ -21,8 +21,12 @@ class Settings(BaseSettings):
     mail_from: EmailStr
     mail_port: int
     mail_server: str
-    rsa_private_key_path: str = "/etc/secrets/private_key.pem"
-    rsa_public_key_path: str = "/etc/secrets/public_key.pem"
+    rsa_private_key_path: str = os.getenv(
+        "RSA_PRIVATE_KEY_PATH", "/etc/secrets/private_key.pem"
+    )
+    rsa_public_key_path: str = os.getenv(
+        "RSA_PUBLIC_KEY_PATH", "/etc/secrets/public_key.pem"
+    )
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
