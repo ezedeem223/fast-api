b5e86012f5c733dbc938621fc1bf4c71e0d9f7fd
ezedeem223 <za9699185@gmail.com>
2024-09-10 10:42:01 +0300

    ezede

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import EmailStr
 
@@ -21,12 +22,23 @@ class Settings(BaseSettings):
     mail_port: int
     mail_server: str
     vault_token: str = "your_vault_token"
-
-    # تغيير هذه السطور لقراءة المفاتيح من المتغيرات البيئية
     rsa_private_key: str
     rsa_public_key: str
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.rsa_private_key = self._read_key_file("private_key.pem")
+        self.rsa_public_key = self._read_key_file("public_key.pem")
+
+    def _read_key_file(self, filename):
+        file_path = os.path.join(os.path.dirname(__file__), "..", filename)
+        try:
+            with open(file_path, "r") as file:
+                return file.read().strip()
+        except FileNotFoundError:
+            raise ValueError(f"Key file not found: {filename}")
 
 
 settings = Settings()
